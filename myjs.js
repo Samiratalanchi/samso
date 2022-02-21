197cf80c15b4418b53e9ae6bd95961d6e401156f
samira talanchi <samira73talanchi@gmail.com>
2022-02-21 16:26:02 +0330

    add menu

---
+++ myjs.js
@@ -0,0 +1,24 @@
+function displayMenu() {
+    if(document.getElementById("second").style.scale != "0" ) {
+        console.log(document.getElementById("second").style.scale);
+        document.getElementById("first").style.transition="1s"
+        document.getElementById("second").style.transition="1s"
+        document.getElementById("third").style.transition="1s"
+        document.getElementById("second").style.scale="0";
+        document.getElementById("first").style.rotate="-135deg";
+        document.getElementById("first").style.marginTop="10px";
+        document.getElementById("third").style.rotate="135deg";
+        document.getElementById("third").style.marginTop="-20px";
+        document.getElementById("navbar-ss").style.width="200px"
+    } else if (document.getElementById("second").style.scale == "0"){
+        document.getElementById("first").style.transition="1s"
+        document.getElementById("second").style.transition="1s"
+        document.getElementById("third").style.transition="1s"
+        document.getElementById("second").style.scale=1;
+        document.getElementById("first").style.rotate="0deg";
+        document.getElementById("first").style.marginTop="5px";
+        document.getElementById("third").style.rotate="0deg";
+        document.getElementById("third").style.marginTop="5px";
+        document.getElementById("navbar-ss").style.width="70px"
+    }
+}
