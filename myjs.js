f9b2eb4627b430e520f1353f504842916bbb8a5c
samira talanchi <samira73talanchi@gmail.com>
2022-02-21 18:16:46 +0330

    add content box

--- myjs.js
+++ myjs.js
@@ -8,7 +8,9 @@ function displayMenu() {
         document.getElementById("first").style.marginTop="10px";
         document.getElementById("third").style.rotate="135deg";
         document.getElementById("third").style.marginTop="-20px";
-        document.getElementById("navbar-ss").style.width="200px"
+        document.getElementById("navbar-ss").style.width="200px";
+        document.getElementById("container").style.width="100vw - 220px";
+        document.getElementById("container").style.marginLeft="220px";
     } else if (document.getElementById("second").style.scale == "0"){
         document.getElementById("first").style.transition="1s"
         document.getElementById("second").style.transition="1s"
@@ -18,6 +20,8 @@ function displayMenu() {
         document.getElementById("first").style.marginTop="5px";
         document.getElementById("third").style.rotate="0deg";
         document.getElementById("third").style.marginTop="5px";
-        document.getElementById("navbar-ss").style.width="75px"
+        document.getElementById("navbar-ss").style.width="75px";
+        document.getElementById("container").style.width="100vw - 90px";
+        document.getElementById("container").style.marginLeft="90px";
     }
 }
