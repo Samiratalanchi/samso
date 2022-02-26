319507ccba63d1ac1ef6a35ef12bd24470647120
samira talanchi <samira73talanchi@gmail.com>
2022-02-26 13:45:27 +0330

    edit menu title visibility

--- myjs.js
+++ myjs.js
@@ -11,6 +11,7 @@ function displayMenu() {
         document.getElementById("navbar-ss").style.width="200px";
         document.getElementById("container").style.width="100vw - 220px";
         document.getElementById("container").style.marginLeft="220px";
+        document.getElementById("mnu-title").style.visibility="visible";
     } else if (document.getElementById("second").style.scale == "0"){
         document.getElementById("first").style.transition="1s"
         document.getElementById("second").style.transition="1s"
@@ -23,5 +24,6 @@ function displayMenu() {
         document.getElementById("navbar-ss").style.width="75px";
         document.getElementById("container").style.width="100vw - 90px";
         document.getElementById("container").style.marginLeft="90px";
+        document.getElementById("mnu-title").style.visibility="hidden";
     }
 }
