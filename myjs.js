39ffca4fdb9b8bc715724364a80baa9d2619145e
samira talanchi <samira73talanchi@gmail.com>
2022-02-26 14:44:03 +0330

    edit button box margin

--- myjs.js
+++ myjs.js
@@ -11,6 +11,7 @@ function displayMenu() {
         document.getElementById("navbar-ss").style.width="200px";
         document.getElementById("container").style.width="100vw - 220px";
         document.getElementById("container").style.marginLeft="220px";
+        document.getElementById("button-box").style.marginLeft="220px";
         document.getElementById("mnu-title").style.visibility="visible";
     } else if (document.getElementById("second").style.scale == "0"){
         document.getElementById("first").style.transition="1s"
@@ -24,6 +25,7 @@ function displayMenu() {
         document.getElementById("navbar-ss").style.width="75px";
         document.getElementById("container").style.width="100vw - 90px";
         document.getElementById("container").style.marginLeft="90px";
+        document.getElementById("button-box").style.marginLeft="90px";
         document.getElementById("mnu-title").style.visibility="hidden";
     }
 }
